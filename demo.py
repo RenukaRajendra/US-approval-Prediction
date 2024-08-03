988dcbcb441251c93e9783946113d04c783ae01c
Renuka Rajendra B <rrb@example.com>
2024-08-03 23:44:10 +0530

    Done

--- demo.py
+++ demo.py
@@ -1,3 +1,9 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
+
+try:
+    a = 1/ 10
+except Exception as e:
+    raise USvisaException(str(e)) from e
 
 logging.info("Welcome to our project!")
